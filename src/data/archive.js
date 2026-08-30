ee0bc8e89d8dc9d27590a05acdacc05c8e404cd2
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-30 18:26:53 +0530

    Add archive data aggregator

---
+++ src/data/archive.js
@@ -0,0 +1,11 @@
+import p1 from './photos-1.js';
+import p2 from './photos-2.js';
+import p3 from './photos-3.js';
+import p4 from './photos-4.js';
+import p5 from './photos-5.js';
+import VIDEOS from './videos.js';
+import VOICES from './voices.js';
+import chapters from './chapters.js';
+
+export const PHOTOS = [...p1,...p2,...p3,...p4,...p5];
+export {VIDEOS, VOICES, chapters};
