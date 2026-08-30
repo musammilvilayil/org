ceeb29096fec3ec6c8d1255fccb156a02c617b00
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-31 01:01:16 +0530

    Wire embedded media and mandatory voices

---
+++ src/embedded/index.js
@@ -0,0 +1,14 @@
+import m0 from './media0.js';
+import m1 from './media1.js';
+import m2 from './media2.js';
+import m3 from './media3.js';
+import v100 from './voice100.js';
+import v101 from './voice101.js';
+import v102 from './voice102.js';
+import v110 from './voice110.js';
+import v111 from './voice111.js';
+import v112 from './voice112.js';
+
+export const MEDIA_B64 = m0 + m1 + m2 + m3;
+export const VOICE10_SRC = 'data:audio/mp4;base64,' + v100 + v101 + v102;
+export const VOICE11_SRC = 'data:audio/mp4;base64,' + v110 + v111 + v112;
