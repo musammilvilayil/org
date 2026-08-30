8f003aba6f58c6531cd1a0e3b66ff04fca51789a
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-31 01:50:30 +0530

    Load curated memory styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import htm from 'htm';
 import App from './App.js';
 import './styles.css';
+import './curation.css';
 
 const html = htm.bind(React.createElement);
 createRoot(document.getElementById('root')).render(html`<${App}/>`);
